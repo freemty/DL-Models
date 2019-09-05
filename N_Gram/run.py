526b9c43da1c0b77c45e8d9601e7b8753d9c195a
freemty <1228154622@qq.com>
2019-09-05 19:49:42 +0800

    commit1

--- N_Gram/run.py
+++ N_Gram/run.py
@@ -1,6 +1,7 @@
 #import SG_Model
 import time
 import string
+import math
 from collections import Counter
 import numpy as np 
 import tensorflow as tf 
@@ -8,23 +9,27 @@ import tensorflow as tf
 
 def loadData(FileName , low_threshold = 10 , high_freq_threshold = 0.85):
 
+    #洗文本
     text = open(FileName).read().lower()
-
     for i in string.punctuation:
         text = text.replace(i , ' ')
-
     text = text.split()
-    
+    #去高频
     wordCounter = Counter(text)
+    text = [word for word in text if wordCounter[word] >= low_threshold]
 
-    
+    high_freq = 1e-3
+    wordCounter = Counter(text)
+    totalCount = len(text)
+    wordFreq = {word : (1 - math.sqrt(high_freq / (count / totalCount)) for word,count in wordCounter.items()}
+    text = [word for word in text if wordFreq[word] < high_freq_threshold]
 
-    vocab = set()
-    vocab = list(vocab)
+    vocab = set(text)
+    vocab_List = list(vocab)
 
-    word2Int = 
-
-    
+    word2Int = {word : index for index , word in enumerate(vocab_List)}
+    int2word = {index : word for index , word in enumerate(vocab_List)}
+    encode = {word2Int[word] for word in text}
     return vocab , word2Int ,int2Word , encode
 
 
--- test/test_emuneart.py
+++ test/test_emuneart.py
@@ -1,6 +1,5 @@
 
 list1 = ['cat' , 'dog' , 'tiger']
 
-
 for index , word in enumerate(list1):
     print(index , word)
